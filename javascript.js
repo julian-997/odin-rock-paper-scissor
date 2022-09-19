ed9e6c9b1db8a65d657e61bae515ada67996591e
Julian <j.ewald97@gmail.com>
2022-09-19 10:08:22 +0200

    Wrote getComputerChoice function.
    Added function to generate a randomized choice for the computer player.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    let computerChoice = "";
+    let randomNumber = Math.floor(Math.random() * 3);
+    
+    if (randomNumber === 0) {
+        computerChoice = "Rock";
+    }
+    if (randomNumber === 1) {
+        computerChoice = "Paper";
+    }
+    if (randomNumber === 2) {
+        computerChoice = "Scissors";
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
