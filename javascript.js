a9439e7dae238b6b839a3138729763a81b81f480
Julian <j.ewald97@gmail.com>
2022-09-19 11:40:15 +0200

    Game is playable. Added output and score counter.

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber === 0) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     }
     if (randomNumber === 1) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
     if (randomNumber === 2) {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
 
     return computerChoice;
@@ -17,13 +17,64 @@ function getComputerChoice() {
 
 function getPlayerSelection() {
     let playerSelection = prompt("Enter your choice: (Rock, Paper, Scissors)");
-    return playerSelection;
+    playerSelection = playerSelection.toLowerCase();
+    if ((playerSelection === "rock") || (playerSelection === "paper") || (playerSelection === "scissors")) {
+        return playerSelection;
+    }
+    else {
+        return "invalid"
+    }
 }
 
-console.log(getComputerChoice());
-console.log(getPlayerSelection());
+function playRound(playerSelection, computerSelection){
+    console.log("Computer: " + computerSelection);
+    console.log("Player: " + playerSelection);
+    if (playerSelection === computerSelection) {
+        return ("It's a draw! You both chose " + playerSelection) + ".";
+    }
 
-function playRound(){
-    let computerSelection = getComputerChoice();
+    else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        return "You win! Rock beats Scissors.";
+    }
+    else if ((playerSelection === "rock") && (computerSelection === "paper")) {
+        return "You lose! Paper beats Rock.";
+    }
+    else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+        return "You win! Paper beats Rock.";
+    }
+    else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
+        return "You lose! Scissors beats Paper.";
+    }
+    else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+        return "You win! Scissors beats Paper.";
+    }
+    else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
+        return "You lose! Rock beats Scissors.";
+    }
+    else {
+        return "The choice you entered was invalid. Try again.";
+    }
 
 }
+
+function game() {
+    let playerScore = 0;
+    let comScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let roundResult = playRound(getPlayerSelection(), getComputerChoice());
+        alert(roundResult);
+        if (roundResult[4] == "w") {        /* If the game returns a win, it is counted here. */
+            playerScore++;
+        }
+        else if (roundResult[4] == "l") {   /* If the game returns a lose, it is counted here. */
+            comScore++;
+        }
+        else if (roundResult[4] == "c") {   /* If the game returns a invalid choice, the current round is not added to the played rounds. */
+            i--;
+        }
+    }
+    
+    alert("Player score: " + playerScore + " - Computer Score: " + comScore);
+}
+
+game();
